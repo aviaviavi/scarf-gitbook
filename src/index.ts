825a292640e6b10acb095c38d30a9190db936785
aviaviavi <avipress@gmail.com>
2025-07-02 14:31:59 -0700

    Addison's changes exactly

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,40 @@
 import {
-    createIntegration,
-    FetchPublishScriptEventCallback,
-    RuntimeContext,
-    RuntimeEnvironment,
+  createIntegration,
+  FetchPublishScriptEventCallback,
+  RuntimeContext,
+  RuntimeEnvironment,
 } from "@gitbook/runtime";
 
-import script from './scarf-script.raw.js';
+import script from "./scarf-script.raw.js";
 
 type ScarfRuntimeContext = RuntimeContext<
-    RuntimeEnvironment<
-        {},
-        {
-            pixel_id?: string
-        }
-    >
+  RuntimeEnvironment<
+    {},
+    {
+      pixel_id?: string;
+    }
+  >
 >;
 
 export const handleFetchEvent: FetchPublishScriptEventCallback = async (
-    event,
-    { environment }: ScarfRuntimeContext,
+  event,
+  { environment }: ScarfRuntimeContext
 ) => {
-    const pixelId = environment.siteInstallation?.configuration?.pixel_id;
+  const pixelId = environment.siteInstallation?.configuration?.pixel_id;
 
-    if (!pixelId) {
-        throw new Error(`The Scarf pixel ID is missing from the configuration.`);
-    }
+  if (!pixelId) {
+    throw new Error(`The Scarf pixel ID is missing from the configuration.`);
+  }
 
-    console.log(script)
-
-    return new Response((script as string).replace('[PIXEL_ID]', pixelId), {
-        headers: {
-            'Content-Type': 'application/javascript',
-            'Cache-Control': 'max-age=604800',
-      },
+  return new Response((script as string).replace("[PIXEL_ID]", pixelId), {
+    headers: {
+      "Content-Type": "application/javascript",
+      "Cache-Control": "max-age=604800",
+    },
   });
 };
 
-
 export default createIntegration<ScarfRuntimeContext>({
-    fetch: handleFetchEvent,
-    fetch_published_script: handleFetchEvent,
+  fetch: handleFetchEvent,
+  fetch_published_script: handleFetchEvent,
 });
